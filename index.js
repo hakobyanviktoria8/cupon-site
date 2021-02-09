b5233788ce7c71160445081eda2cdbbb5d63cebc
Viktoria <v.hakobyan@voodoo.pro>
2021-02-09 13:26:40 +0400

    mobile carent navbar

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
 //carusel slick
 $(document).ready(function (){
-
+    $(".carusel0").slick({
+        dots: false,
+        infinite: false,
+        autoplay: false,
+        arrows: false,
+        speed: 300,
+        slidesToShow: 9,
+        slidesToScroll: 1,
+        variableWidth: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 7,
+                    slidesToScroll: 1,
+                    infinite: false,
+                    dots: false,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 5,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
     $(".carusel1").slick({
         dots: true,
         infinite: true,
@@ -16,8 +50,6 @@ $(document).ready(function (){
         infinite: true,
         autoplay: false,
         arrows: false,
-        // prevArrow : '<a href="" class="arrow left2"><i class="fas fa-chevron-left"></i></a>',
-        // nextArrow : '<a href="" class="arrow right2"><i class="fas fa-chevron-right"></i></a>',
         speed: 300,
         slidesToShow: 7,
         slidesToScroll: 1,
