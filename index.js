42c20fba23d09c8f25fe374766c66f37e635e55b
Viktoria <v.hakobyan@voodoo.pro>
2021-02-09 16:48:02 +0400

    label, mobile changes

--- index.js
+++ index.js
@@ -66,14 +66,14 @@ $(document).ready(function (){
             {
                 breakpoint: 600,
                 settings: {
-                    slidesToShow: 3,
+                    slidesToShow: 4,
                     slidesToScroll: 1
                 }
             },
             {
                 breakpoint: 480,
                 settings: {
-                    slidesToShow: 2,
+                    slidesToShow: 3,
                     slidesToScroll: 1
                 }
             }
