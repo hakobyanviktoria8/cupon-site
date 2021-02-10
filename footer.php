3c60f2b1b0196daf5138e1d7ed308ff1f46271a0
Viktoria <v.hakobyan@voodoo.pro>
2021-02-10 16:28:01 +0400

    header.php & footer.php

---
+++ footer.php
@@ -0,0 +1,86 @@
+
+<!--footer-->
+<div class="footer-top py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-6 col-xs-12 text-white">
+                <img src="images/header-logo.png" alt="">
+                <p class="footer-top-p">сленг</p>
+                <h3 class="">Контакты</h3>
+                <hr class="text-white">
+                <div class="my-2">
+                    <a href="#" class="footer-top-icons"><i class="fas fa-map-marker-alt"></i></a>
+                    <span>Р.А. г. Ереван ул. Екмалян 6</span>
+                </div>
+                <div class="my-2">
+                    <a href="#" class="footer-top-icons"><i class="fas fa-phone-alt"></i></a>
+                    <span>Тел։ (096) 500 183; (010) 505 506;</span>
+                    <p class="footer-top-phone">(096) 500 183; (010) 505 506</p>
+                </div>
+                <div class="my-2">
+                    <a href="#" class="footer-top-icons"><i class="fas fa-envelope"></i></a>
+                    <span>РА. Ереван ул. Екмалян 6</span>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-xs-12 text-white">
+                <h3 class="text-uppercase">О Biglemon</h3>
+                <hr>
+                <div  class="my-2"><a href="#">Как это работает</a></div>
+                <div class="my-2"><a href="#">О компании</a> </div>
+                <div class="my-2"><a href="#">Блог</a> </div>
+                <div class="my-2"><a href="#">Новости</a></div>
+                <div class="my-2"><a href="#">Вакансии</a></div>
+                <div class="my-2"><a href="#">Правила</a></div>
+                <div class="my-2"><a href="#">Связ</a></div>
+                <div class="my-2"><a href="#">Связ</a></div>
+                 <div class="footer-top-bank">
+                     <a href=""> <img src="images/Layer%20525.png" alt=""></a>
+                     <a href=""><img src="images/mobidram_logo.png" alt=""></a>
+                     <a href=""><img src="images/Layer%20526.png" alt=""></a>
+                     <a href=""><img src="images/unnamed%20(1).png" alt=""></a>
+                     <a href=""><img src="images/easy.png" alt=""></a>
+                 </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-xs-12 text-white">
+                <h3 class="text-uppercase">Для бизнеса</h3>
+                <hr>
+                <div class="my-2"><a href="#">Для вашего бизнеса</a></div>
+                <div class="my-2"><a href="#">Вопросы и ответы</a> </div>
+                <div class="my-2"><a href="#">Все акции</a> </div>
+                <div class="my-2"><a href="#">Прошедшие акции</a> </div>
+
+                <h3 class="text-uppercase mt-5">Следите за нами</h3>
+                <hr>
+                <p>Следите за нами в социальных сетях, чтобы быть в  курсе последн</p>
+                <div class="footer-bootom-icons">
+                    <span><i class="fab fa-facebook-f"></i></span>
+                    <span><i class="fab fa-twitter"></i></span>
+                    <span><i class="fab fa-instagram"></i></span>
+                    <span><i class="fab fa-google-plus-g"></i></span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="footer-bootom py-4">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-6 col-xs-12 text-white text-uppercase">© 2020 BigLemon. Все права защищены</div>
+            <div class="col-lg-4 col-md-6 col-xs-12 text-white text-uppercase">Разработчик: VOODOo PROGRaMMING
+                <img class="pb-2" src="images/voodoo-doll.png" alt="">
+            </div>
+        </div>
+    </div>
+</div>
+<button onclick="topFunction()" id="myBtn" title="Go to top"><i class="fa fa-angle-up"></i></button>
+
+<!--carusel slick-->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script type="text/javascript" src="slick-1.8.1/slick/slick.min.js"></script>
+
+<script src="index.js?v=1"></script>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Biglemon</title>
+
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+          integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <script src="https://use.fontawesome.com/5957e40b5e.js"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Tangerine">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!-- favicon error  -->
+    <link rel="shortcut icon" href="#">
+
+    <!--    carusel slick-->
+    <link rel="stylesheet" type="text/css" href="slick-1.8.1/slick/slick.css"/>
+    <link rel="stylesheet" type="text/css" href="slick-1.8.1/slick/slick-theme.css"/>
+
+    <link rel="stylesheet" href="index.css">
+
+</head>
+<body>
+
+<!--flags and icons-->
+<div class="flagsIcons">
+    <div class="container p-0">
+        <div class="row m-0">
+            <div class="col-lg-12 p-0">
+                <span class="navbar_linkIcons">
+                    <a href="#"><img src="images/facebook.png" alt=""></a>
+                    <a href="#"><img src="images/instagram.png" alt=""></a>
+                    <a href="#"><img src="images/twitter.png" alt=""></a>
+                    <a href="#"><img src="images/odnoklassniki.png" alt=""></a>
+                </span>
+                <span class="navbar_linkFlags">
+                    <a href="#"><img src="images/arm.jpg" alt=""></a>
+                    <a href="#"><img src="images/ru.jpg" alt=""></a>
+                </span>
+            </div>
+        </div>
+    </div>
+</div>
+
+ <!--header-->
+<div class="headerWraper">
+    <div class="header py-3">
+        <div class="container">
+        <!--header-top-->
+            <div class="row mb-2 header-top">
+                <div class="col-lg-7 col-md-0 p-0">
+                    <a href="tel: +374-96-500-183" class="m-0 text-dark">(096/093) 500 183, </a>
+                    <a href="tel: +374-10-505-506" class="m-0 text-dark">(010) 505 506, </a>
+                    <a href="tel: +374-93-500-173" class="m-0 text-dark">(093/060) 500 173, </a><br>
+                    <a href="tel: +374-10-505-507" class="m-0 text-dark">(010) 505 507, </a>
+                    <a href="tel: +374-55-336-070" class="m-0 text-dark">(055) 336 070 </a>
+                </div>
+                <!-- Помощь -->
+                <div class="col-xs-6 col-sm-6 col-md-6 col-lg-2  p-0">
+                    <div class="dropdown help">
+                        <button class="btn  dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <img src="images/header-icon1.png" class="pr-2" alt=""> Помощь
+                        </button>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <ul>
+                                <a class="dropdown-item" href="#">
+                                    <li><p class="m-0">Lorem ipsum dolor.</p></li>
+                                    <small>Lorem ipsum dolor sit amet.</small>
+                                </a>
+                                <a class="dropdown-item" href="#">
+                                    <li><p class="m-0">Lorem ipsum dolor.</p></li>
+                                    <small>Lorem ipsum dolor sit amet.</small>
+                                </a>
+                                <a class="dropdown-item" href="#">
+                                    <li><p class="m-0">Lorem ipsum dolor.</p></li>
+                                    <small>Lorem ipsum dolor sit amet.</small>
+                                </a>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-6 col-sm-6  col-md-6 col-lg-3 my-2 p-0">
+                    <img src="images/header-icon2.png" alt="" class="pr-2">
+                    <span class="text-dark">ДЛЯ ВАШЕГО БИЗНЕСА</span>
+                </div>
+            </div>
+
+          <!--header row 2-->
+            <div class="row  my-3 header-bottom">
+                <div class="col-lg-3 col-md-2 p-0">
+                    <img class = "bigLemon logo" src="images/footer-logo.png" alt="">
+                    <img class = "lemon logo" src="images/home-icon8.png" alt="">
+                </div>
+                <div class="col-lg-6 col-md-8 border col2 p-0">
+                    <form action="">
+                        <input class="pl-3" type="text" placeholder="Найти среди 400 акций">
+                        <button><i class="fas fa-search"></i></button>
+                    </form>
+                </div>
+                <div class="col-lg-3 col-md-2 p-0 text-right col3">
+                    <a href="#" class="registration">Вход / Регистрация</a>
+                    <a href="#" class="userIcon"><i class="fas fa-user"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+   <!--2navbar-->
+    <div class="container p-0">
+       <!--carusel0-->
+        <div class="row m-0">
+            <div class="col-12 p-0">
+                <section class="carusel0">
+                    <div class="py-2 ml-1">
+                        <a class="text-uppercase" href="#">ТОП АКЦИИ</a>
+                    </div>
+                    <div class="py-2 ml-1">
+                        <a class="text-uppercase" href="#">Все 280</a>
+                    </div>
+                    <div class="py-2 ml-1">
+                        <a class="text-uppercase" href="#">услуги</a>
+                    </div>
+                    <div class="py-2 ml-1">
+                        <a class="text-uppercase" href="#">отели</a>
+                    </div>
+                    <div class="py-2 ml-1">
+                        <a class="text-uppercase" href="#">еда</a>
+                    </div>
+                    <div class="py-2 ml-1">
+                        <a class="text-uppercase" href="#">промокоды</a>
+                    </div>
+                    <div class="py-2 ml-1">
+                        <a class="text-uppercase" href="#">товары</a>
+                    </div>
+                    <div class="py-2 ml-1">
+                        <a class="text-uppercase" href="swimming-pool.html">бассейны</a>
+                    </div>
+                    <div class="py-2 ml-1">
+                        <a class="text-uppercase" href="#">КАРТА</a>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </div>
+</div>
