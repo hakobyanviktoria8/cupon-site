b9f1fbaa72caf7274d3266d8c0122219b71e8eb3
Viktoria <v.hakobyan@voodoo.pro>
2021-03-05 18:22:00 +0400

    about + mobile

---
+++ about.php
@@ -0,0 +1,180 @@
+<?php include('header.php')?>
+
+<div class="container">
+    <div class="row m-0">
+        <div class="col-12 p-0">
+            <div class="category-path">Главная / Бассейны / Product</div>
+        </div>
+    </div>
+    <!--carusel1-->
+    <div class="row">
+        <div class="col-12">
+            <section class="carusel1 slider">
+                <div><img src="images/home-ads1.jpg"></div>
+                <div><img src="images/home-ads1.jpg"></div>
+                <div><img src="images/home-ads1.jpg"></div>
+                <div><img src="images/home-ads1.jpg"></div>
+            </section>
+        </div>
+    </div>
+    <div class="row border-top mb-3 subTitleWraper mx-0">
+        <div class="col-12 p-0">
+            <span class="subTitle">О компании</span>
+        </div>
+    </div>
+
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto corporis cumque est illum ipsum iste laudantium officiis quas, repellendus reprehenderit rerum sint sit temporibus vitae voluptate. Ea illum iure quae?</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eligendi eos exercitationem facilis optio porro qui sapiente sed soluta ut.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eligendi eos exercitationem facilis optio porro qui sapiente sed soluta ut.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eligendi eos exercitationem facilis optio porro qui sapiente sed soluta ut.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eligendi eos exercitationem facilis optio porro qui sapiente sed soluta ut.</p>
+
+    <div class="row">
+        <div class="col-12">
+            <h5>Почему купить на Biglemon?</h5>
+        </div>
+    </div>
+    <div class="row my-5">
+        <div class="col-12 col-md-6 col-lg-4  text-center">
+            <div class="row">
+                <div class="col-12 mb-4">
+                    <img src="images/home-icon10.png" alt="">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <p><b>Впечатления любому настроению</b></p>
+                    <p class="mb-1">Lorem ipsum dolor sit amet, consectetur.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-md-6 col-lg-4  text-center">
+            <div class="row">
+                <div class="col-12 mb-4">
+                    <img src="images/home-icon10.png" alt="">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <p><b>Впечатления любому настроению</b></p>
+                    <p class="mb-1">Lorem ipsum dolor sit amet, consectetur.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-md-6 col-lg-4  text-center">
+            <div class="row">
+                <div class="col-12 mb-4">
+                    <img src="images/home-icon10.png" alt="">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <p><b>Впечатления любому настроению</b></p>
+                    <p class="mb-1">Lorem ipsum dolor sit amet, consectetur.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut dolore doloremque, doloribus eaque iste magni mollitia nostrum perferendis perspiciatis quidem! Autem dolorum ipsam minima porro quae quos sapiente temporibus tenetur totam voluptas. Architecto aut consequuntur cumque dolorum earum eum, excepturi laborum laudantium libero molestias nulla quo rem repellendus saepe voluptatum.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque est et exercitationem illum, molestias nisi numquam pariatur reiciendis rem ut vitae voluptatem! Dolor in iusto non quisquam soluta. Exercitationem, officia!</p>
+    <b>Lorem ipsum dolor sit amet.</b>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto aspernatur assumenda distinctio dolor dolore enim excepturi explicabo harum impedit in omnis quae quidem quo ratione sapiente tempore, temporibus vitae voluptate.</p>
+</div>
+
+    <!--bg green carusel4-->
+    <div class="green-BG">
+        <div class="container-fluid ">
+            <div class="row">
+                <div class="col-12">
+                    <div class="carusel4 py-2">
+                        <div class="carusel4-wrap">
+                            <img src="images/home-icon8.png" alt="">
+                            <h2 class="text-uppercase font-weight-bold">4.163.700.000 ДРАМ</h2>
+                            <p class="text-uppercase m-0"><b>экономия на годы</b></p>
+                            <p class="text-uppercase m-0"><b>факт: экономия на годы 1.222.444.000</b></p>
+                        </div>
+                        <div class="carusel4-wrap">
+                            <img src="images/home-icon8.png" alt="">
+                            <h2 class="text-uppercase font-weight-bold">4.163.700.000 ДРАМ</h2>
+                            <p class="text-uppercase m-0"><b>экономия на годы</b></p>
+                            <p class="text-uppercase m-0"><b>факт: экономия на годы 1.222.444.000</b></p>
+                        </div>
+                        <div class="carusel4-wrap">
+                            <img src="images/home-icon8.png" alt="">
+                            <h2 class="text-uppercase font-weight-bold">4.163.700.000 ДРАМ</h2>
+                            <p class="text-uppercase m-0"><b>экономия на годы</b></p>
+                            <p class="text-uppercase m-0"><b>факт: экономия на годы 1.222.444.000</b></p>
+                        </div>
+                        <div class="carusel4-wrap">
+                            <img src="images/home-icon8.png" alt="">
+                            <h2 class="text-uppercase font-weight-bold">4.163.700.000 ДРАМ</h2>
+                            <p class="text-uppercase m-0"><b>экономия на годы</b></p>
+                            <p class="text-uppercase m-0"><b>факт: экономия на годы 1.222.444.000</b></p>
+                        </div>
+                        <div class="carusel4-wrap">
+                            <img src="images/home-icon8.png" alt="">
+                            <h2 class="text-uppercase font-weight-bold">4.163.700.000 ДРАМ</h2>
+                            <p class="text-uppercase m-0"><b>экономия на годы</b></p>
+                            <p class="text-uppercase m-0"><b>факт: экономия на годы 1.222.444.000</b></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<div class="container adresses my-5">
+    <div class="row">
+        <h3 class="pl-3">Ботинки и пинетки</h3>
+    </div>
+    <div class="row">
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-xs-6 col-6 mb-4">
+            <a href="">Ботинки и пинетки - 10 разноцветные</a><br>
+            <a href="">Ботинки и пинетки - 10 розовые</a><br>
+            <a href="">Повседневный - 10 серии</a><br>
+            <a href="">Квартиры - 10, Зелёная</a><br>
+            <a href="">Квартиры - 10, Серебро</a><br>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-xs-6 col-6 mb-4">
+            <a href="">Ботинки и пинетки - 10 разноцветные</a><br>
+            <a href="">Ботинки и пинетки - 10 розовые</a><br>
+            <a href="">Повседневный - 10 серии</a><br>
+            <a href="">Квартиры - 10, Зелёная</a><br>
+            <a href="">Квартиры - 10, Серебро</a><br>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-xs-6 col-6 mb-4">
+            <a href="">Ботинки и пинетки - 10 разноцветные</a><br>
+            <a href="">Ботинки и пинетки - 10 розовые</a><br>
+            <a href="">Повседневный - 10 серии</a><br>
+            <a href="">Квартиры - 10, Зелёная</a><br>
+            <a href="">Квартиры - 10, Серебро</a><br>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-xs-6 col-6 mb-4">
+            <a href="">Ботинки и пинетки - 10 разноцветные</a><br>
+            <a href="">Ботинки и пинетки - 10 розовые</a><br>
+            <a href="">Повседневный - 10 серии</a><br>
+            <a href="">Квартиры - 10, Зелёная</a><br>
+            <a href="">Квартиры - 10, Серебро</a><br>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-xs-6 col-6 mb-4">
+            <a href="">Ботинки и пинетки - 10 разноцветные</a><br>
+            <a href="">Ботинки и пинетки - 10 розовые</a><br>
+            <a href="">Повседневный - 10 серии</a><br>
+            <a href="">Квартиры - 10, Зелёная</a><br>
+            <a href="">Квартиры - 10, Серебро</a><br>
+        </div>
+        <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-xs-6 col-6 mb-4">
+            <a href="">Ботинки и пинетки - 10 разноцветные</a><br>
+            <a href="">Ботинки и пинетки - 10 розовые</a><br>
+            <a href="">Повседневный - 10 серии</a><br>
+            <a href="">Квартиры - 10, Зелёная</a><br>
+            <a href="">Квартиры - 10, Серебро</a><br>
+        </div>
+    </div>
+</div>
+
+<?php include('footer.php')?>
