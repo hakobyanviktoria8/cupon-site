7dbee26b94031c15e208674ebff6576e7c287c0e
Viktoria <v.hakobyan@voodoo.pro>
2021-03-05 19:14:18 +0400

    how it work

---
+++ how-it-work.php
@@ -0,0 +1,59 @@
+<?php include('header.php')?>
+
+<div class="container">
+    <div class="row m-0">
+        <div class="col-12 p-0">
+            <div class="category-path">Главная / Бассейны / Product</div>
+        </div>
+    </div>
+    <!--carusel1-->
+    <div class="row">
+        <div class="col-12">
+            <section class="carusel1 slider">
+                <div><img src="images/home-ads1.jpg"></div>
+                <div><img src="images/home-ads1.jpg"></div>
+                <div><img src="images/home-ads1.jpg"></div>
+                <div><img src="images/home-ads1.jpg"></div>
+            </section>
+        </div>
+    </div>
+    <div class="row border-top mb-3 subTitleWraper mx-0">
+        <div class="col-12 p-0">
+            <span class="subTitle">как это работает?</span>
+        </div>
+    </div>
+    <b>Lorem ipsum dolor sit amet.</b>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto corporis cumque est illum ipsum iste laudantium officiis quas, repellendus reprehenderit rerum sint sit temporibus vitae voluptate. Ea illum iure quae?</p>
+</div>
+
+<!--bg green carusel4-->
+<div class="green-bg">
+    <div class="container how_it_work">
+        <h5>Why Biglemon</h5>
+        <div class="row">
+            <div class="col-12">
+                <div class="carusel4 py-2">
+                    <div class="carusel4-wrap mx-3">
+                        <h2>Прибыль</h2>
+                        <h3>ot 250 000 dram</h3>
+                        <small>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aperiam debitis ea excepturi inventore molestiae necessitatibus obcaecati quos voluptate voluptates?</small>
+                    </div>
+                    <div class="carusel4-wrap mx-3">
+                        <h2>Прибыль</h2>
+                        <h3>ot 250 000 dram</h3>
+                        <small>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aperiam debitis ea excepturi inventore molestiae necessitatibus obcaecati quos voluptate voluptates?</small>
+                    </div>
+                    <div class="carusel4-wrap mx-3">
+                        <h2>Прибыль</h2>
+                        <h3>ot 250 000 dram</h3>
+                        <small>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aperiam debitis ea excepturi inventore molestiae necessitatibus obcaecati quos voluptate voluptates?</small>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<?php include('footer.php')?>
