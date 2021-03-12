5437a5e8b6fa176a7bec0bcc06b7a68a2594b8d3
Viktorya Hakobyan <hakobyanviktorya8@gmail.com>
2021-03-12 13:32:45 +0400

    for business + mobile

---
+++ for-business-open.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Biglemon</title>
+
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+          integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <script src="https://use.fontawesome.com/5957e40b5e.js"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Tangerine">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!-- favicon error  -->
+    <link rel="shortcut icon" href="#">
+
+    <link rel="stylesheet" href="CSS/for-business-open.css">
+
+</head>
+<body>
+
+    <div class="for_business_open">
+        <div class="for_business_header">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 text-right">
+                        <span>Lorem ipsum dolor sit amet.</span>
+                        <a href="">вход</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-8">
+                    <h3>Lorem ipsum dolor sit amet.</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic, reiciendis?</p>
+                    <h4>Lorem ipsum dolor.</h4>
+                </div>
+                <div class="col-4"></div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ for-business.php
@@ -0,0 +1,63 @@
+<?php include('header.php')?>
+
+<div class="container mb-5">
+    <div class="row m-0">
+        <div class="col-12 p-0">
+            <div class="category-path">Главная / Бассейны / Product</div>
+        </div>
+    </div>
+    <!--carusel1-->
+    <div class="row">
+        <div class="col-12">
+            <section class="carusel1 slider">
+                <div><img src="images/home-ads1.jpg"></div>
+                <div><img src="images/home-ads1.jpg"></div>
+                <div><img src="images/home-ads1.jpg"></div>
+                <div><img src="images/home-ads1.jpg"></div>
+            </section>
+        </div>
+    </div>
+    <div class="row border-top mb-3 subTitleWraper mx-0">
+        <div class="col-12 p-0">
+            <span class="subTitle">Для вашего бизнеса</span>
+        </div>
+    </div>
+</div>
+
+<div class="for_business">
+    <div class="for_business_row_wraper">
+        <div class="for_business_col text-center">
+            <div><img src="images/marketing.png" alt=""></div>
+            <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi aut consequatur consequuntur deleniti deserunt doloremque et hic impedit incidunt, labore necessitatibus porro, possimus quibusdam quis quisquam totam velit. Provident, tempora.</p>
+        </div>
+        <div class="for_business_col">
+            <img src="images/Rectangle22.png" alt="">
+        </div>
+    </div>
+    <div class="for_business_row_wraper">
+        <div class="for_business_col">
+            <img src="images/Rectangle22.png" alt="">
+        </div>
+        <div class="for_business_col text-center">
+            <div><img src="images/marketing.png" alt=""></div>
+            <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi aut consequatur consequuntur deleniti deserunt doloremque et hic impedit incidunt, labore necessitatibus porro, possimus quibusdam quis quisquam totam velit. Provident, tempora.</p>
+        </div>
+    </div>
+    <div class="for_business_row_wraper">
+        <div class="for_business_col text-center">
+            <div><img src="images/marketing.png" alt=""></div>
+            <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi aut consequatur consequuntur deleniti deserunt doloremque et hic impedit incidunt, labore necessitatibus porro, possimus quibusdam quis quisquam totam velit. Provident, tempora.</p>
+        </div>
+        <div class="for_business_col">
+            <img src="images/Rectangle22.png" alt="">
+        </div>
+    </div>
+
+    <h4 class="text-center my-5 mx-3">Узнайте, как Biglemon может работать на вас</h4>
+    <div class="text-center link mb-5"><a href="for-business-open.php" class="text-center">Lorem</a></div>
+</div>
+
+<?php include('footer.php')?>
